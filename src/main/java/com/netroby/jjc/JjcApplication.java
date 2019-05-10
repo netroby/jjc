6663e29ae7108e7c5dd9715f2848557867ebff7a
ZhiFeng Hu <hufeng1987@gmail.com>
2019-05-10 09:55:59 +0800

    Add java code

---
+++ src/main/java/com/netroby/jjc/JjcApplication.java
@@ -0,0 +1,13 @@
+package com.netroby.jjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjcApplication.class, args);
+	}
+
+}
